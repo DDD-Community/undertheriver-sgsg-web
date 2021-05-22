42b73c78a5e79a47be3965c58ace6bc32c294888
siyeon <siyeonleeme@gmail.com>
2021-05-22 15:22:37 +0900

    feat: add router and chakra provider config

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Main } from './containers/index';
+import PrivateRoute from './pages/PrivateRoute';
+import Login from './pages/Login';
+import AfterLogin from './pages/AfterLogin';
+import Setting from './pages/Setting';
+import NotFound from './pages/NotFound';
 
 const history = createMemoryHistory();
 
 const Root: React.FC = () => (
-  <Router history={history}>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
-  </Router>
+  <ChakraProvider resetCSS>
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/after-login" component={AfterLogin} />
+        <PrivateRoute exact path="/" component={Main} />
+        <PrivateRoute exact path="/setting" component={Setting} />
+        <Route path="/error" component={NotFound} />
+        <Redirect path="*" to="/" /> *
+      </Switch>
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 export default Root;
