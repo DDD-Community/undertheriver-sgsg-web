8f99b03991d7d0b01fad2972bfd33d6cf410dc62
siyeon <siyeonleeme@gmail.com>
2021-05-29 10:14:18 +0900

    feat: fix folder UI

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -12,18 +12,75 @@ const squareLine = css`
   border-top: 0.125rem solid white;
 `;
 
+const folderWrapper = css`
+  //display: flex;
+  //flex-direction: column;
+`;
+
+const headerWrapper = css`
+  width: 2rem;
+  height: 0.625rem;
+  border-radius: 0.125rem;
+  color: black;
+  z-index: 10;
+  position: absolute;
+  &.BLUE {
+    background-color: #2da5d7;
+  }
+
+  &.GREEN {
+    background-color: #00a09b;
+  }
+
+  &.RED {
+    background-color: #e64632;
+  }
+
+  &.ORANGE {
+    background-color: #f99a42;
+  }
+
+  &.PURPLE {
+    background-color: #9f69db;
+  }
+`;
+
+const boxWrapper = css`
+  width: 1.75rem;
+  height: 2rem;
+  //margin-top: -0.5rem;
+  margin-left: 0.125rem;
+  padding-top: 0.75rem;
+  border-radius: 0.125rem;
+  background-color: #6b6862;
+  &.BLUE {
+    background-color: #7ec0dc;
+  }
+
+  &.GREEN {
+    background-color: #74b9b7;
+  }
+
+  &.RED {
+    background-color: #de8d83;
+  }
+
+  &.ORANGE {
+    background-color: #f5bd89;
+  }
+
+  &.PURPLE {
+    background-color: #bb9adf;
+  }
+`;
+
 function Folder({ color }) {
   return (
-    <div
-      css={{
-        width: '2.5rem',
-        height: '2.5rem',
-        background: `${color}`,
-        paddingTop: '0.75rem',
-        borderRadius: '0.125rem',
-      }}
-    >
-      <hr css={squareLine} />
+    <div class={folderWrapper}>
+      <div css={headerWrapper} className={color}></div>
+      <div css={boxWrapper} className={color}>
+        <hr css={squareLine} />
+      </div>
     </div>
   );
 }
