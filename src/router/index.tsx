8d07adf910d29bc15f169a2d178ae38b5c1f0da6
siyeon <sylee@megazone.com>
2021-07-10 02:59:56 +0900

    refactor: project structure

---
+++ src/router/index.tsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React from 'react';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+import AfterLogin from '../pages/AfterLogin';
+import PrivateRoute from './PrivateRoute';
+import Main from '../pages/main';
+import Setting from '../pages/Setting';
+import NotFound from '../pages/NotFound';
+
+const history = createBrowserHistory();
+
+const Router = () => {
+  return (
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/after-login" component={AfterLogin} />
+        <PrivateRoute exact path="/" component={Main} />
+        <PrivateRoute exact path="/setting" component={Setting} />
+        <Route path="/error" component={NotFound} />
+        <Redirect path="*" to="/" /> *
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
