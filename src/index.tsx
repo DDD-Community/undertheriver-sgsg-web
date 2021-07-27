4c3a000f2909f892121862e8626040adb12ceabe
siyeon <sylee@megazone.com>
2021-07-28 02:12:22 +0900

    feat: install swr and remove react-query

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,12 @@ import './styles/index.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider resetCSS>
-        <App />
-      </ChakraProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ChakraProvider resetCSS>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
